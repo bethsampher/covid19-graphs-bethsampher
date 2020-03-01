54397ab9eee77436f72b0e007b712b54558c6ae9
bethsampher <bs15@sanger.ac.uk>
2020-03-01 20:28:20 +0000

    Wrote failing test for download_from_github #3
    ==================================================================================== FAILURES ====================================================================================
    ___________________________________________________________________________ test_download_from_github ____________________________________________________________________________
    
        def test_download_from_github():
            with patch('requests.get') as mock_get:
                mock_get.return_value.text = 'Test info'
                mock_get.return_value.status_code = 200
                processing = Covid19Processing('test_dir')
                response = processing.download_from_github()
    >           assert response == 200
    E           assert None == 200
    
    covid19_graphs/tests/test_covid19_processing.py:19: AssertionError
    ========================================================================== 1 failed, 5 passed in 0.21s ===========================================================================

--- covid19_graphs/tests/test_covid19_processing.py
+++ covid19_graphs/tests/test_covid19_processing.py
@@ -1,6 +1,6 @@
 """ unit tests for covid19_processing """
 
-import mock
+from mock import patch
 import pytest
 
 from covid19_graphs.covid19_processing import Covid19Processing
@@ -9,4 +9,15 @@ from covid19_graphs.covid19_processing import Covid19Processing
 def test_out_dir():
     processing_with_dir = Covid19Processing('test_dir')
     assert processing_with_dir.out_dir == 'test_dir'
+
+def test_download_from_github():
+    with patch('requests.get') as mock_get:
+        mock_get.return_value.text = 'Test info'
+        mock_get.return_value.status_code = 200
+        processing = Covid19Processing('test_dir')
+        response = processing.download_from_github()
+        assert response == 200
+        assert processing.data == 'Test info'
+        assert processing.status_code == 200
+
 
