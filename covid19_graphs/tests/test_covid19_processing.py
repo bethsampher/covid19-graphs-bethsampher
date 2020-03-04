9d3b2d71b629d07a81c242b8bbbf0b42cfde8546
bethsampher <bs15@sanger.ac.uk>
2020-03-04 18:27:54 +0000

    Wrote test for process_data #11
    ==================================================================================== FAILURES ====================================================================================
    _______________________________________________________________________________ test_process_data ________________________________________________________________________________
    
        def test_process_data():
            processing = Covid19Processing('test_dir')
            test_data = 'State,Country/Region,1/22/20,3/3/20\nAnhui,Mainland China,1,990\nBeijing,Mainland China,14,414\n,UK,0,51'
            processing.data = test_data
            processing.process_data()
            expected_rows = [[15, 0, 15], [1404, 51, 1455]]
            expected_col_names = ['China', 'Other', 'Total']
            expected_row_names = ['1/22/20', '3/3/20']
            expected_csv_data = pd.DataFrame(data=expected_rows, columns=expected_col_names, index=expected_row_names)
    >       assert expected_csv_data.equals(processing.csv_data)
    E       AttributeError: 'Covid19Processing' object has no attribute 'csv_data'
    
    covid19_graphs/tests/test_covid19_processing.py:33: AttributeError
    ========================================================================== 1 failed, 6 passed in 0.64s ===========================================================================

--- covid19_graphs/tests/test_covid19_processing.py
+++ covid19_graphs/tests/test_covid19_processing.py
@@ -1,6 +1,7 @@
 """ unit tests for covid19_processing """
 
 from mock import patch
+import pandas as pd
 import pytest
 
 from covid19_graphs.covid19_processing import Covid19Processing
@@ -20,4 +21,14 @@ def test_download_from_github():
         assert processing.data == 'Test info'
         assert processing.status_code == 200
 
+def test_process_data():
+    processing = Covid19Processing('test_dir')
+    test_data = 'State,Country/Region,1/22/20,3/3/20\nAnhui,Mainland China,1,990\nBeijing,Mainland China,14,414\n,UK,0,51'
+    processing.data = test_data
+    processing.process_data()
+    expected_rows = [[15, 0, 15], [1404, 51, 1455]]
+    expected_col_names = ['China', 'Other', 'Total']
+    expected_row_names = ['1/22/20', '3/3/20']
+    expected_csv_data = pd.DataFrame(data=expected_rows, columns=expected_col_names, index=expected_row_names)
+    assert expected_csv_data.equals(processing.csv_data)
 
